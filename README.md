b56ee146fb4f8b47d8ff5b43c97d1a765d4bfe92
unknown <qitas@qitas.cn>
2018-11-17 17:41:34 +0800

    add install

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# Gitlab
-
-[![sites](docs/Q.png)](http://www.qitas.cn)
-
-## 描述
+﻿# [Gitlab](https://github.com/qitas/Gitlab) 
 
 基于容器和dockerfile部署代码管理平台
+
+### qitas@qitas.cn
+
+---
 
 ## 组成
 
 
-## 说明
+---
 
+## 为锻造最美之器
 
-
-[![docker Logo](docs/docker.png)](http://www.os-q.com)
+[![sites](qitas/qitas.png)](http://www.qitas.cn)
--- buildDocker.sh
+++ buildDocker.sh
@@ -9,8 +9,8 @@ sudo docker build -t  qitas/gitlab ./gitlab/
 
 sudo docker run -itd  \
     -p 443:443  -p 80:80  -p 222:22 \
-    --restart always \
-    --volume /home/gitlab/config:/etc/gitlab \
-    --volume /home/gitlab/logs:/var/log/gitlab \
-    --volume /home/gitlab/data:/var/opt/gitlab \
+    --restart=always \
+    --volume /home/gitlab/config:/etc/gitlab    \
+    --volume /home/gitlab/logs:/var/log/gitlab  \
+    --volume /home/gitlab/data:/var/opt/gitlab  \
     --name qithub qitas/gitlab 
